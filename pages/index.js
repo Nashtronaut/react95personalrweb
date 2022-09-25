52a8ffc0b59eb0e81f224a830f4eeaab8611a17b
Nash Boisvert <fenderfan@live.ca>
2022-09-25 17:55:01 -0600

    added windows favicon

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
       <Head>
         <title>Nash Boisvert</title>
         <meta name="Nash Boisvert's personal website and information" content="Information about Nash Boisvert including contact, passed projects, and resume. Built with Next.JS and react95." />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
